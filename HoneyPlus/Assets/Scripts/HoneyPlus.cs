80afecbc5555208ee3042986f9ca251688b8aa7a
Loz <laurence.ure@gmail.com>
2021-03-08 02:10:39 +0000

    Filled out rough draft of recipes and initial base class for the mod

---
+++ HoneyPlus/Assets/Scripts/HoneyPlus.cs
@@ -0,0 +1,12 @@
+﻿using BepInEx;
+
+namespace HoneyPlus
+{
+    [BepInPlugin(modGuid, modName, modVer)]
+    public class HoneyPlus : BaseUnityPlugin
+    {
+        public const string modGuid = "Loz." + modName;
+        private const string modName = "HoneyPlus";
+        private const string modVer = "1.0.0";
+    }
+}
