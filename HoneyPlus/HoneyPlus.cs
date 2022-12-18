ef941c5803430b3930ca3b49571e4b3b7bf1e1b0
Loz <laurence.ure@gmail.com>
2022-12-18 02:37:47 +0000

    3.0.7 - Fixed missing Serpent texture
    Fixed this by reverting to the uncooked serpent meat texture from the old valheim rip, this won't help in the future or with the new magical mushroom stew that throws errors in the console regarding shaders

--- HoneyPlus/HoneyPlus.cs
+++ HoneyPlus/HoneyPlus.cs
@@ -17,7 +17,7 @@ namespace HoneyPlus
   {
     public const string PluginGUID = "OhhLoz-HoneyPlus";
     public const string PluginName = "HoneyPlus";
-    public const string PluginVersion = "3.0.6";
+    public const string PluginVersion = "3.0.7";
 
     private const string AssetBundleName = "honeyplusassets";
     private const string RecipeFileName = "recipes.json";
