bdec4ffff68bcf8d01ae00486e6b0ebc217e11ff
Loz <laurence.ure@gmail.com>
2023-02-11 19:57:35 +0000

    5.2.0 - Added x10 crafting versions of Tar & RoyalJelly alongside x4 Sausages

--- HoneyPlus/HoneyPlus.cs
+++ HoneyPlus/HoneyPlus.cs
@@ -18,7 +18,7 @@ namespace HoneyPlus
   {
     public const string PluginGUID = "OhhLoz-HoneyPlus";
     public const string PluginName = "HoneyPlus";
-    public const string PluginVersion = "5.1.2";
+    public const string PluginVersion = "5.2.0";
 
     // FILES
     private const string AssetBundleName = "honeyplusassets";
